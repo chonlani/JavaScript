e9b3eedd77f8188d7d3df9ed6b98db378c137a0d
chonlani <53692121+chonlani@users.noreply.github.com>
2020-02-24 15:40:09 +0700

    fix1

---
+++ AdvancedJavascript-21Feb63.js
@@ -0,0 +1,73 @@
+// let user = {
+//     name : "jang",
+    
+//     "computer skill": "Javascript"
+// }
+
+// user.age = 18;
+// user.height = 156;
+// console.log(user);
+// console.log(user["computer skill"]);
+
+// delete user.name;
+// console.log(user);
+
+
+// let human ={};
+//     human.name = "jang";
+//     human.age = 15;
+//     human.group = "p'nat";
+//     human.IsSingle = true;
+//     human.score = 5;
+
+// console.log(human);
+
+// let key = prompt("name")
+// let value = prompt("value")
+// let obj = {}
+// obj [key] = value;
+// obj ["age"] = 19;y
+// console.log(obj);
+
+//แบบฝึกหัด
+// let obj = {}
+
+// while(true){
+//     let propertyname = prompt("name");
+//     if (propertyname == "stop") break;
+//     let propertyvalue = prompt("value");
+//     obj[propertyname] = propertyvalue
+
+// }
+// console.log(obj);
+
+// let obj = {}
+// let n1 = "firstname";
+
+// obj1[n1]="jang";
+// obj2["lastname"]="jung";
+// obj3["age"]=25;
+// obj4["height"]=158;
+
+// console.log(obj);
+
+//แบบฝึกหัด
+//4.7. computer propertie
+let obj = {}
+let key = prompt("fruit");
+let value = Number(prompt("how many"));
+
+   while(true){
+    let key = prompt("fruit");
+    if(value > 1){
+    key = key +"s";
+    // key= `${key}s`;
+    
+    }
+   }
+obj[key]=value;
+console.log(obj);
+
+
+
+
---
+++ AdvancedJavascript-24Feb63.js
@@ -0,0 +1,109 @@
+let sum = 0;
+for (let i = 1 ; i <= 165 ; i = i+2 ){
+    sum += i;
+}
+console.log(sum);
+
+let propertiesName = "age";
+
+let obj = {
+  [propertiesName]: 20, // จะได้ key เป็น propertiesName
+}
+
+console.log(obj);
+
+const obj ={
+    name: "jang",
+    age: 25,
+    height: 165,
+    weight: 47,
+};
+
+let clone1={};
+
+for (let key in obj){
+     clone[key] = obj[key];
+
+}
+
+console.log(clone1,obj);
+ogj.name="bee";
+console.log(clone,obj);
+
+//แบบฝึกหัด
+const obj = {
+    name: "sonter",
+    surname: "pakron",
+    name: "Boy",
+};
+ 
+delete obj.name;
+
+console.log(obj);
+
+//ช้อ2
+function isEmpty(obj){
+    let isEmpty = true;
+    for (let key in obj){
+        return false;   // isEmpty = false;
+    }
+    //return true;  
+    return isEmpty;
+
+}
+/*let obj = {
+    name: "sonetr"
+}*/
+
+console.log(obj);
+
+//ช้อ3
+const user = {
+    name : "john"
+};
+
+user.name = "pete";
+
+//not work
+console.log(user.name);
+
+//ช้อ4
+let salaries = {
+    john : 100,
+    ann : 160,
+    pete : 130
+
+}
+let sum = 0;
+function sum (salaries){
+    for (let key in salaries)
+    sum += salaries
+    return sum;
+}
+sum(salaries);
+
+// function sum(obj){
+//     let totel =0;
+//     for(let key in salaries){
+//         total += obj[key];
+//     }
+//     return total;
+// }
+console.log(sum[salarise]);
+
+//ข้อ5
+function multiplynumberic(obj, times){
+    for (let key in obj){
+        if (typeof (obj[key]) == "number"){
+            obj[key] *= times
+        }
+    }
+
+    return obj;
+}
+
+let team={
+    A: 100,
+    B: 200
+};
+console.log(multiplynumberic(team,5)); //5 คือเลขที่นำไปคูณกับvalue
